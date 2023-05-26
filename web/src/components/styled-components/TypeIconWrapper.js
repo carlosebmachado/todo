a7f1e726b1a06e27033e49ec3cd09fc482e182b0
Carlos Eduardo de Borba Machado <carlosebmachado@outlook.com>
2023-05-25 22:04:07 -0300

    Create TypeIconWrapper.js

---
+++ web/src/components/styled-components/TypeIconWrapper.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import constants from "../../constants";
+
+export default styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  /* margin-left: 10px; */
+  padding: 4px;
+  background-color: ${constants.colors.primary};
+  border-radius: 50%;
+  color: white;
+`;
