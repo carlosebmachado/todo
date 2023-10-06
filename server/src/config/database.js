7ffbc6292f28b7f4b485475e8b7611b94e9ed0a1
Carlos Eduardo de Borba Machado <carlosebmachado@outlook.com>
2023-10-06 13:33:17 -0300

    connect mongodb

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb://127.0.0.1:27017/todo';
+// const URI = 'mongodb://127.0.0.1:27017/todo';
+const URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@atlascluster.qqmavpm.mongodb.net/todo?retryWrites=true&w=majority`;
 mongoose.connect(URI, { useNewUrlParser: true });
 
 module.exports = mongoose;
