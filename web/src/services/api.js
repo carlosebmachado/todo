9bdb21d7e0e32620387347040659f730628b2f76
Carlos Eduardo de Borba Machado <carlosebmachado@outlook.com>
2023-10-05 22:14:57 -0300

    web: add auth token

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
-export default axios.create({
-  baseURL: 'http://localhost:3333'
-});
+export default function api(token) {
+  return axios.create({
+    baseURL: 'http://localhost:3333',
+    headers: {
+      'Authorization': `Bearer ${token}`,
+    },
+  });
+}
