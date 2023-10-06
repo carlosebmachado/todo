6241ce3a0aac51928138c487f797062509625bc5
Carlos Eduardo de Borba Machado <carlosebmachado@outlook.com>
2023-10-05 22:11:35 -0300

    configure db dev conn

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/todo';
-mongoose.connect(url, {useNewUrlParser: true});
+const URI = 'mongodb://127.0.0.1:27017/todo';
+mongoose.connect(URI, { useNewUrlParser: true });
 
 module.exports = mongoose;
