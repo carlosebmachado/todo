3592a1c3b424f62dd78be37c46c6dd75aa8805cb
Carlos Eduardo de Borba Machado <carlosebmachado@outlook.com>
2023-10-08 16:25:47 -0300

    adding dotenv

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 
 export default function api(token) {
   return axios.create({
-    //baseURL: 'http://localhost:3333',
-    baseURL: 'https://todo-backend-services.onrender.com',
+    baseURL: process.env.ENVIRONMENT === 'production' ? 'https://todo-backend-services.onrender.com' : 'http://localhost:3333',
     headers: {
       'Authorization': `Bearer ${token}`,
     },
