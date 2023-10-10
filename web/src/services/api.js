8c7e2ae168e6690df21edfdd9118327003fe3c5f
Carlos Eduardo de Borba Machado <carlosebmachado@outlook.com>
2023-10-10 12:54:45 -0300

    web: refact api configs

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,10 +1,15 @@
 import axios from 'axios';
 
 export default function api(token) {
-  return axios.create({
+  var reqConfig = {
     baseURL: process.env.ENVIRONMENT === 'production' ? 'https://todo-backend-services.onrender.com' : 'http://localhost:3333',
-    headers: {
+  };
+
+  if (token) {
+    reqConfig.headers = {
       'Authorization': `Bearer ${token}`,
-    },
-  });
+    };
+  }
+
+  return axios.create(reqConfig);
 }
