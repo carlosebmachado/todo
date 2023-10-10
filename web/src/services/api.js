6e43885148c1a14f0c537af1e0be003a434ab24d
Carlos Eduardo de Borba Machado <carlosebmachado@outlook.com>
2023-10-10 09:40:35 -0300

    Update api.js

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default function api(token) {
   return axios.create({
-    baseURL: process.env.ENVIRONMENT === 'production' ? 'https://todo-backend-services.onrender.com' : 'http://localhost:3333',
+    baseURL: process.env.ENVIRONMENT == 'development' ? 'http://localhost:3333' : 'https://todo-backend-services.onrender.com',
     headers: {
       'Authorization': `Bearer ${token}`,
     },
